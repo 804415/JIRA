4f0b7b7d53ed18908ab559d2c801c32f290ef0cd
775477 <775477@VATLODCD00120.delta.rl.delta.com>
2017-01-16 14:51:39 +0530

    initial

---
+++ FlightCountMaven/FlightCountJAR/src/main/java/org/FlightCountJAR/App.java
@@ -0,0 +1,13 @@
+package org.FlightCountJAR;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
